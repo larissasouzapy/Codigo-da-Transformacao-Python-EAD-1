e69158a4d26b9460b4853e4350b747da59eaa9fd
Aluno Vocação: Larissa Souza <larissacarneirodesouzarios@gmail.com>
2026-08-24 19:20:22 -0300

    Update and rename modulo02/none.py to modulo02/modulo02/modulo02_intro_python_larissasouza-ativ02.py

---
+++ modulo02/modulo02/modulo02_intro_python_larissasouza-ativ02.py
@@ -0,0 +1,7 @@
+print("Olá, Mundo! Estou testando o Python.")
+print(42)
+
+
+print("O tipo de 'Olá, Mundo' é:", type("Olá, Mundo"))  
+print("O tipo de 55 é:", type(55))                     
+print("O tipo de 5.5 é:", type(5.5)
