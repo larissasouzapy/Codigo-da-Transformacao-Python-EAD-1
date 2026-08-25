4ecf386f19ef95737438a512883e616eccf18583
Aluno Vocacao <larissacarneirodesouzarios@gmail.com>
2026-08-25 13:34:14 -0300

    update

--- modulo02/modulo02/modulo02/modulo02/modulo02_intro_python_larissa-desafio.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-nome = input("Por favor, digite o seu nome: ")
-
-hora_atual = datetime.now().strftime("%H:%M:%S")
-
-print(f"Olá, {nome}! Agora são exatamente {hora_atual}.")
--- modulo02/modulo02/modulo02/modulo02_intro_python_larissa-ativ03.py
+++
@@ -1,3 +0,0 @@
-nome = input("Oiê! Digite o seu nome: ")
-
-print(f"Prazer, {nome}! Tudo bem?")
