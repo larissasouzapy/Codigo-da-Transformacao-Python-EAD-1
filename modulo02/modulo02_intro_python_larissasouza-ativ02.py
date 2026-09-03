f1cdcef2bd95e05111117f1a48c3ec42cf6cd5a6
Aluno Vocacao <larissacarneirodesouzarios@gmail.com>
2026-09-03 15:30:25 -0300

    upgrade

--- modulo02/modulo02_intro_python_larissasouza-ativ02.py
+++ modulo02/modulo02_intro_python_larissasouza-ativ02.py
@@ -1,7 +1,7 @@
-print("Olá, Mundo! Estou testando o Python.")
-print(55)
 
+print("Olá, Mundo!")
 
-print("O tipo de 'Olá, Mundo' é:", type("Olá, Mundo"))  
-print("O tipo de 55 é:", type(55))                     
-print("O tipo de 5.5 é:", type(5.5))
+print("Tipo de 'Olá, Mundo!':", type("Olá, Mundo!"))
+print("Tipo de 10:", type(10))
+print("Tipo de 3.14:", type(3.14))
+print("Tipo de True:", type(True))
